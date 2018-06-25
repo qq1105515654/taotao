054fb5033597ba139622c466a25dd4f8f6bf21ee
盛年华 <1105515654@qq.com>
2018-06-25 15:48:03 +0800

    整个后台管理，构建完毕

---
+++ taotao-manager/taotao-manager-controller/src/main/java/com/taotao/TaotaoManagerControllerApplication.java
@@ -0,0 +1,12 @@
+package com.taotao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaotaoManagerControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaotaoManagerControllerApplication.class, args);
+    }
+}
